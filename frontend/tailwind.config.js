518b1bead81f9f2df91a47e90fed9ccbf0291912
Sohila Metwaly <103280614+sohilametwaly@users.noreply.github.com>
2024-05-13 05:03:28 +0300

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+import plugin from 'tailwindcss/plugin'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -20,5 +22,9 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant("current", "&.active");
+    }),
+  ],
 };
